14740f9f92cf2ada1b87e9fce96f132387d2248f
Crislana Rafael <crislanarafael@gmail.com>
2022-07-31 16:57:43 -0700

    Preliminary homepage/about page
    Note Login and Sign Up links are using routing so I think if you should be able to add your views and change the routing to your vue frontend for login and signup it should work (hopefully)!

---
+++ codehouse-vue/src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>Study 2gether</h1>
+    <p>
+      Women are often times excluded in conversations relating to technology,
+      and often times there are very few women in these spaces especially
+      classrooms, to begin with. This website will provide students with a place
+      to come together and be heard, as well as find other women in stem to
+      study with based on similar coursework.
+    </p>
+    <router-link to="/about"><button>Login</button></router-link> |
+    <router-link to="/about"><button>Sign Up</button></router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+button {
+  background-color: #3A405A;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>
